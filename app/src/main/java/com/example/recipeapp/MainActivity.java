0d7073974e503c7060690e849681a1af20422464
Bernhard <b.nordemann@aol.com>
2022-01-15 02:35:48 -0500

    added login page

--- app/src/main/java/com/example/recipeapp/MainActivity.java
+++ app/src/main/java/com/example/recipeapp/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.recipeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String GET_USERNAME = "com.example.recipeapp.USERNAME";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view){
+        Intent intent = new Intent(this, DisplayRecipes.class);
+        EditText editText = (EditText) findViewById(R.id.username_box);
+        String username = editText.getText().toString();
+        intent.putExtra(GET_USERNAME, username);
+        startActivity(intent);
     }
 }
