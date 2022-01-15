92d340bcb54f80feeb0db2e480087644240387f2
Bernhard <b.nordemann@aol.com>
2022-01-15 04:39:58 -0500

    added recipe list and onclick functionality

---
+++ app/src/main/java/com/example/recipeapp/CustomAdapter.java
@@ -0,0 +1,73 @@
+package com.example.recipeapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder>{
+    private final ArrayList<String> recipeAuthor;
+    private ArrayList<String> recipeNames;
+    private Context context;
+    private OnRecipeListener onRecipeListener;
+
+    public CustomAdapter(Context context, ArrayList<String> recipeNames, ArrayList<String> recipeAuthor, OnRecipeListener onRecipeListener){
+        this.recipeNames = recipeNames;
+        this.recipeAuthor = recipeAuthor;
+        this.context = context;
+        this.onRecipeListener = onRecipeListener;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        TextView text;
+        OnRecipeListener onRecipeListener;
+        public ViewHolder(@NonNull View itemView, OnRecipeListener onRecipeListener) {
+            super(itemView);
+            text = itemView.findViewById(R.id.textView);
+            this.onRecipeListener = onRecipeListener;
+            itemView.setOnClickListener(this);
+        }
+        @Override
+        public void onClick(View view) {
+            onRecipeListener.onRecipeClick(getAdapterPosition());
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.layout_listitem, viewGroup, false);
+
+        return new ViewHolder(view, onRecipeListener);
+    }
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        //viewHolder.getTextView().setText(localDataSet[position]);
+
+        viewHolder.text.setText(recipeNames.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipeNames.size();
+    }
+
+    public interface OnRecipeListener{
+        void onRecipeClick(int position);
+    }
+
+
+}
--- app/src/main/java/com/example/recipeapp/DisplayRecipes.java
+++ app/src/main/java/com/example/recipeapp/DisplayRecipes.java
@@ -1,13 +1,19 @@
 package com.example.recipeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
-public class DisplayRecipes extends AppCompatActivity {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
+public class DisplayRecipes extends AppCompatActivity implements CustomAdapter.OnRecipeListener{
+    private ArrayList<String> recipeNames;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +24,26 @@ public class DisplayRecipes extends AppCompatActivity {
         Intent intent = getIntent();
         String username = intent.getStringExtra(MainActivity.GET_USERNAME);
 
+        recipeNames = new ArrayList<>();
 
-
+        for(int i = 0; i < 25; i++){
+            recipeNames.add(""+i);
+        }
+        initRecyclerView();
     }
 
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.recipe_list);
+        CustomAdapter adapter = new CustomAdapter(this, recipeNames, recipeNames, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
 
-
-
-
+    @Override
+    public void onRecipeClick(int position) {
+        System.out.println(recipeNames.get(position));
+//        recipeNames.get(position);
+//        Intent intent = new Intent(this, NewActivity--);
+//        startActivity(intent);
+    }
 }
